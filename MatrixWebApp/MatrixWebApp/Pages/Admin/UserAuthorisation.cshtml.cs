9c8f4506fe08758d105eb557d7a7305af1277f40
bjorn <bkraussen@gmail.com>
2025-06-03 22:23:06 +0200

    API Fix

--- MatrixWebApp/MatrixWebApp/Pages/Admin/UserAuthorisation.cshtml.cs
+++ MatrixWebApp/MatrixWebApp/Pages/Admin/UserAuthorisation.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net.Http;
 using System.Net.Http.Json;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class UserAuthorisationModel : PageModel
 {
@@ -9,6 +11,7 @@ public class UserAuthorisationModel : PageModel
     private readonly ILogger<UserAuthorisationModel> _logger;
 
     public List<UserViewModel> Users { get; set; } = new();
+    public List<RoleDto> Roles { get; set; } = new();
 
     public UserAuthorisationModel(IHttpClientFactory httpClientFactory, ILogger<UserAuthorisationModel> logger)
     {
@@ -26,12 +29,19 @@ public class UserAuthorisationModel : PageModel
             {
                 Users = users;
             }
+            Roles = await _httpClient.GetFromJsonAsync<List<RoleDto>>("api/Role");
         }
         catch (HttpRequestException ex)
         {
-            _logger.LogError(ex, "Error retrieving users from API.");
+            _logger.LogError(ex, "Error retrieving users or roles from API.");
             Users = new List<UserViewModel>();
+            Roles = new List<RoleDto>();
         }
+    }
+
+    public string GetRoleName(int roleId)
+    {
+        return Roles.Find(r => r.RoleId == roleId)?.RoleName ?? $"Onbekend ({roleId})";
     }
 
     public class UserViewModel
@@ -39,6 +49,12 @@ public class UserAuthorisationModel : PageModel
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public int RoleId { get; set; }
+    }
+
+    public class RoleDto
+    {
+        public int RoleId { get; set; }
         public string RoleName { get; set; }
     }
 }
