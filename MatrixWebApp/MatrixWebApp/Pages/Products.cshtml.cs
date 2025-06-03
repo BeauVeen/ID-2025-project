ba08020827702e7a4e6d5faa60d4af520f1f8021
jayden2064859 <jayden.rerimassie@gmail.com>
2025-06-03 19:15:23 +0200

    producten pagina layout veranderd, begin aan sidebar gemaakt

--- MatrixWebApp/MatrixWebApp/Pages/Products.cshtml.cs
+++ MatrixWebApp/MatrixWebApp/Pages/Products.cshtml.cs
@@ -14,19 +14,21 @@ namespace MatrixWebApp.Pages
         private readonly ILogger<ProductsModel> _logger;
 
         public List<ProductDto> Products { get; set; } = new();
+        public List<CategoryDto> Categories { get; set; } = new(); 
 
         public ProductsModel(IHttpClientFactory httpClientFactory, ILogger<ProductsModel> logger)
-        { 
+        {
             _httpClient = httpClientFactory.CreateClient("MatrixApi");
             _logger = logger;
         }
 
         public async Task OnGetAsync()
         {
+            ViewData["ShowProductsSideBar"] = true; 
+
             try
             {
                 var products = await _httpClient.GetFromJsonAsync<List<ProductDto>>("api/Product");
-
                 if (products != null)
                 {
                     Products = products;
@@ -35,23 +37,38 @@ namespace MatrixWebApp.Pages
                 {
                     _logger.LogWarning("No products received from API.");
                 }
+
+                var categories = await _httpClient.GetFromJsonAsync<List<CategoryDto>>("api/Category");
+                if (categories != null)
+                {
+                    Categories = categories;
+                }
+                else
+                {
+                    _logger.LogWarning("No categories received from API.");
+                }
             }
             catch (HttpRequestException ex)
             {
-                _logger.LogError(ex, "Error with retrieving products from API.");
+                _logger.LogError(ex, "Error retrieving data from API.");
             }
         }
 
-
         public class ProductDto
-        { 
+        {
             public int ProductId { get; set; }
             public int CategoryId { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Price { get; set; }
-            public int Stock {  get; set; }
+            public int Stock { get; set; }
             public byte[] Picture { get; set; }
+        }
+
+        public class CategoryDto 
+        {
+            public int CategoryId { get; set; }
+            public string Name { get; set; }
         }
     }
 }
