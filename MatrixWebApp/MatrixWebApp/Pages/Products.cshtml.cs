7378b3d36d6055ded6828e633036aa18b6ea5b1e
jayden2064859 <jayden.rerimassie@gmail.com>
2025-06-04 19:35:27 +0200

    shoppingcart toegevoegd

--- MatrixWebApp/MatrixWebApp/Pages/Products.cshtml.cs
+++ MatrixWebApp/MatrixWebApp/Pages/Products.cshtml.cs
@@ -5,6 +5,8 @@ using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using MatrixWebApp.Models;
+using MatrixWebApp.Extensions;
 
 namespace MatrixWebApp.Pages
 {
@@ -22,6 +24,37 @@ namespace MatrixWebApp.Pages
             _logger = logger;
         }
 
+        public async Task<IActionResult> OnPostAddToCart(int productId, int quantity)
+        {
+            // Haal productdetails op van de API
+            var product = await _httpClient.GetFromJsonAsync<ProductDto>($"api/Product/{productId}");
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Haal huidige winkelwagen op of maak nieuwe
+            var cart = HttpContext.Session.Get<ShoppingCart>("Cart") ?? new ShoppingCart();
+
+            // Voeg item toe
+            cart.AddItem(new CartItem
+            {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Price = product.Price,
+                Picture = product.Picture,
+                Quantity = quantity
+            });
+
+            // Sla winkelwagen op in session
+            HttpContext.Session.Set("Cart", cart);
+            HttpContext.Session.SetInt32("CartItemCount", cart.TotalItems);
+
+            // Terug naar productpagina met succesmelding
+            TempData["SuccessMessage"] = $"{product.Name} is toegevoegd aan je winkelwagen!";
+            return RedirectToPage();
+        }
         public async Task OnGetAsync()
         {
             try
