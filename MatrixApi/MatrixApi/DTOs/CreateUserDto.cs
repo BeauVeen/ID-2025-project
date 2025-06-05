ac0e26c763eb78495e0f1c2425b3f27906625c4d
jayden2064859 <jayden.rerimassie@gmail.com>
2025-06-05 19:25:28 +0200

    layout gefixed

--- MatrixApi/MatrixApi/DTOs/CreateUserDto.cs
+++ MatrixApi/MatrixApi/DTOs/CreateUserDto.cs
@@ -2,8 +2,9 @@
 {
     public class CreateUserDto
     {
+      
         public string Password { get; set; } = null!;
-        public int RoleId { get; set; }
+        public int? RoleId { get; set; }
         public string Name { get; set; } = null!;
         public string? Address { get; set; }
         public string? Zipcode { get; set; }
