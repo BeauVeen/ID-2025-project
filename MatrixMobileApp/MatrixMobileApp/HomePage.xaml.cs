349d59aa3585d0e1b3c2185690e798319ef30252
jayden2064859 <jayden.rerimassie@gmail.com>
2025-06-17 10:54:56 +0200

    Update

--- MatrixMobileApp/MatrixMobileApp/HomePage.xaml.cs
+++ MatrixMobileApp/MatrixMobileApp/HomePage.xaml.cs
@@ -76,6 +76,7 @@ namespace MatrixMobileApp
                 // Container logica hier nog toevoegen
                 // zoals alle orders van de container toevoegen aan actieve orders pagina
                 // Orders moeten gemarkeerd worden als 'Onderweg' wanneer bezorger container scant en op 'Begin rit' klikt (Begin Rit 2.2.0 design in Trello)
+                // Er moet een optimale route voor deze orders gegenereerd worden op de Route pagina
 
             }
 
