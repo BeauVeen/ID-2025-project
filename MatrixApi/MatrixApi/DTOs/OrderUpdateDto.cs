eda28c81b52279f35206602e5c803628858cc1fb
BeauVeen <beauvdveen@pm.me>
2025-06-17 17:55:22 +0200

    new file:   MatrixApi/MatrixApi/Controllers/ContainerController.cs 	modified:   MatrixApi/MatrixApi/Controllers/OrderController.cs 	new file:   MatrixApi/MatrixApi/DTOs/ContainerCreateDto.cs 	modified:   MatrixApi/MatrixApi/DTOs/OrderCreateDto.cs 	new file:   MatrixApi/MatrixApi/DTOs/OrderUpdateDto.cs 	modified:   MatrixApi/MatrixApi/Data/AppdbContext.cs 	new file:   MatrixApi/MatrixApi/Models/Container.cs 	new file:   MatrixApi/MatrixApi/Models/ContainerOrder.cs 	modified:   MatrixApi/MatrixApi/Models/Order.cs 	modified:   MatrixApi/MatrixApi/Program.cs 	new file:   MatrixApi/MatrixApi/Services/ContainerService.cs 	modified:   MatrixApi/MatrixApi/Services/OrderService.cs 	modified:   MySQLSetup.sql

---
+++ MatrixApi/MatrixApi/DTOs/OrderUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MatrixApi.DTOs
+{
+    public class OrderUpdateDto
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public string Status { get; set; }
+        public IFormFile? Signature { get; set; }
+    }
+}
