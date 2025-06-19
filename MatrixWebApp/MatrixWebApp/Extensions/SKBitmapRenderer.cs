2646db1beb956d97de60e2113e637fb3530cc456
BeauVeen <beauvdveen@pm.me>
2025-06-19 13:58:03 +0200

    new file:   MatrixWebApp/MatrixWebApp/Extensions/SKBitmapRenderer.cs 	modified:   MatrixWebApp/MatrixWebApp/MatrixWebApp.csproj 	new file:   MatrixWebApp/MatrixWebApp/Pages/Admin/BarcodeCreator.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Admin/BarcodeCreator.cshtml.cs 	modified:   MatrixWebApp/MatrixWebApp/Pages/Shared/_Layout.cshtml

---
+++ MatrixWebApp/MatrixWebApp/Extensions/SKBitmapRenderer.cs
@@ -0,0 +1,45 @@
+﻿using SkiaSharp;
+using ZXing;
+using ZXing.Common;
+using ZXing.Rendering;
+
+namespace MatrixWebApp.Extensions
+{
+    public class SKBitmapRenderer : IBarcodeRenderer<SKBitmap>
+    {
+        public SKBitmap Render(BitMatrix matrix, BarcodeFormat format, string content, EncodingOptions options)
+        {
+            int width = matrix.Width;
+            int height = matrix.Height;
+
+            var bitmap = new SKBitmap(width, height);
+            using var canvas = new SKCanvas(bitmap);
+            canvas.Clear(SKColors.White);
+
+            var paint = new SKPaint
+            {
+                Color = SKColors.Black,
+                Style = SKPaintStyle.Fill
+            };
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (matrix[x, y])
+                    {
+                        canvas.DrawPoint(x, y, paint);
+                    }
+                }
+            }
+
+            return bitmap;
+        }
+
+        // Extra overload voor de interface
+        public SKBitmap Render(BitMatrix matrix, BarcodeFormat format, string content)
+        {
+            return Render(matrix, format, content, new EncodingOptions());
+        }
+    }
+}
